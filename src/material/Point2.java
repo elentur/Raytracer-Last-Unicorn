f3d6f7389859555e8a1ba1683c2c56d89d0e0328
schubalibre <robertodziuba@googlemail.com>
2015-12-20 21:53:05 +0100

    sampling Klasse angelegt

---
+++ src/material/Point2.java
@@ -0,0 +1,14 @@
+package material;
+
+/**
+ * Created by roberto on 20/12/15.
+ */
+public class Point2 {
+    public final double x;
+    public final double y;
+
+    public Point2(final double x, final double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/sampling/SamplingPattern.java
@@ -0,0 +1,7 @@
+package sampling;
+
+/**
+ * Created by roberto on 20/12/15.
+ */
+public class SamplingPattern {
+}
