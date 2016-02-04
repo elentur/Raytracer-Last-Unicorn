ec1360508434cee60f8a6dad3b7e576e84ad5d13
Marcus Bätz <s59156@beuth-hochschule.de>
2016-02-04 14:38:43 +0100

    Merge remote-tracking branch 'remotes/origin/robert' into marcus
    # Conflicts:
    #	src/controller/NodeSettingsViewController.java

--- src/controller/MainMaterialSettingsController.java
+++ src/controller/MainMaterialSettingsController.java
@@ -8,7 +8,6 @@ import javafx.beans.property.ObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
