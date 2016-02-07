1fe7e34560869408ca720c628c6f6654b6b58aee
schubalibre <robertodziuba@googlemail.com>
2016-02-07 22:55:19 +0100

    Camera eingaben überprüfen

--- src/observables/cameras/AOCamera.java
+++ src/observables/cameras/AOCamera.java
@@ -26,6 +26,60 @@ public abstract class AOCamera extends AOElement {
     public final DoubleProperty ty = new SimpleDoubleProperty(1);
     public final DoubleProperty tz = new SimpleDoubleProperty(0);
 
+    public AOCamera(){
+        tx.addListener(a->{
+            checkT();
+        });
+
+        ty.addListener(a->{
+            checkT();
+        });
+
+        tz.addListener(a->{
+            checkT();
+        });
+
+        gx.addListener(a->{
+            checkG();
+        });
+
+        gy.addListener(a->{
+            checkG();
+        });
+
+        gz.addListener(a->{
+            checkG();
+        });
+
+        ex.addListener(a->{
+            checkE();
+        });
+
+        ey.addListener(a->{
+            checkE();
+        });
+
+        ez.addListener(a->{
+            checkE();
+        });
+
+    }
+
+    private void checkT(){
+        if(tx.get() == 0 && ty.get() == 0 && tz.get() == 0)
+            ty.set(1);
+    }
+
+    private void checkG(){
+        if(gx.get() == 0 && gy.get() == 0 && gz.get() == 0)
+            gy.set(1);
+    }
+
+    private void checkE(){
+        if(ex.get() == 0 && ey.get() == 0 && ez.get() == 0)
+            ey.set(1);
+    }
+
     public final IntegerProperty patternSubdiv = new SimpleIntegerProperty(1);
 
     public abstract Camera generate();
